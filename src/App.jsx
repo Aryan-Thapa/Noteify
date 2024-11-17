add5c0b0c9373dcc1b7b49d63a9f9448a7045e04
Aryan-Thapa <aryanthapa219@gmail.com>
2024-11-17 16:51:31 +0530

    Created a basic route structure for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import Notes from "./components/Notes";
+import NotesView from "./components/NotesView";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <NavBar />
+        <HomePage />
+      </div>
+    ),
+  },
+  {
+    path: "/notes",
+    element: (
+      <div>
+        <NavBar />
+        <Notes />
+      </div>
+    ),
+  },
+  {
+    path: "/notes/:id",
+    element: (
+      <div>
+        <NavBar />
+        <NotesView />
+      </div>
+    ),
+  },
+]);
 
 function App() {
-
-  return (
-    <div>
-      Hello
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
